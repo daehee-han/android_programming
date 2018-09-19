3aed1a30982dc37f2ecded3aa3edbcd02ff2cfd2
daehee <daehee@pedium.com>
2018-09-19 13:17:07 +0900

    setalarm 예제

---
+++ setalarm/app/src/main/java/com/kmu/setalarm/MainActivity.java
@@ -0,0 +1,35 @@
+package com.kmu.setalarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.provider.AlarmClock;
+import java.util.Calendar;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Calendar now = Calendar.getInstance(); // 현재 시각
+        now.add(Calendar.MINUTE, 1); // 1분 후 시각
+        int hour = now.get(Calendar.HOUR);
+        int minute = now.get(Calendar.MINUTE);
+        Log.i("Cal", "hour=" + hour);
+        Log.i("Cal", "minute=" + minute);
+        createAlarm("잠을 깨자 !!", hour, minute);
+    }
+
+    public void createAlarm(String message, int hour, int minutes) {
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                .putExtra(AlarmClock.EXTRA_MESSAGE, message)
+                .putExtra(AlarmClock.EXTRA_HOUR, hour)
+                .putExtra(AlarmClock.EXTRA_MINUTES, minutes);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+}
