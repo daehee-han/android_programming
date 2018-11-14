8360dd97cfdbfd2e0389be6c24fb74cfe2ca26d3
daehee <daehee@pedium.com>
2018-11-14 14:40:03 +0900

    chap16/SensorTest1

---
+++ chap16.vogella/SensorTest1/app/src/main/java/kr/co/company/SensorTest1/SensorTest1.java
@@ -0,0 +1,30 @@
+package kr.co.company.SensorTest1;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class SensorTest1 extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		String report = "";
+		SensorManager manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		List<Sensor> sensors = manager.getSensorList(Sensor.TYPE_ALL);
+		report += "전체 센서수: " + sensors.size() + "\n";
+		int i = 0;
+		for (Sensor s : sensors) {
+			report += "" + i++ + " name: " + s.getName() + "\npower: "
+					+ s.getPower() + "\nres: " + s.getResolution()
+					+ "\nrange: " + s.getMaximumRange() + "\n\n";
+		}
+		TextView text = (TextView) findViewById(R.id.text);
+		text.setText(report);
+	}
+}
