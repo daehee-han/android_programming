e2c1367d81ecc41331a8ca6cb98832ed5401ecb7
daehee <daehee@pedium.com>
2018-11-26 14:07:59 +0900

    WebDownload - gson

--- chap13/WebDownload/app/src/main/java/kr/co/company/webdownload/MainActivity.java
+++ chap13/WebDownload/app/src/main/java/kr/co/company/webdownload/MainActivity.java
@@ -19,6 +19,60 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+class Student {
+
+	@SerializedName("name")
+	@Expose
+	private String name;
+	@SerializedName("email")
+	@Expose
+	private String email;
+	@SerializedName("phone")
+	@Expose
+	private String phone;
+	@SerializedName("age")
+	@Expose
+	private Integer age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+}
+
 public class MainActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -32,7 +86,8 @@ public class MainActivity extends Activity {
 				if (isNetworkAvailable()) {
 					EditText url = (EditText) findViewById(R.id.url);
 					DownloadTask downloadTask = new DownloadTask();
-					downloadTask.execute(url.getText().toString());
+					//downloadTask.execute(url.getText().toString());
+					downloadTask.execute("http://10.0.2.2:9001/api/students");
 
 				} else {
 					Toast.makeText(getBaseContext(),
@@ -99,6 +154,14 @@ public class MainActivity extends Activity {
 			Toast.makeText(getBaseContext(),
 					"Web page downloaded successfully", Toast.LENGTH_SHORT)
 					.show();
+
+			Log.i("RESPONSE", result);
+			//Gson gson = new Gson();
+			Gson gson = new GsonBuilder()
+					.setLenient()
+					.create();
+			Student [] students = gson.fromJson("[{\"name\": \"kim\", \"email\": \"kim@gmail.com\", \"phone\": \"010-1234-0001\", \"age\": 10}]", Student[].class);
+			Log.i("STUDENTS", students[0].getEmail());
 		}
 	}
 }
